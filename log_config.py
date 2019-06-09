e2c126111611a0d52420475a15fa7f8b285973e1
ganlanshu <grapebook0@163.com>
2019-06-09 14:44:15 +0800

    modify log config

--- log_config.py
+++ log_config.py
@@ -7,15 +7,17 @@ import datetime
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
-LOG_FILE = 'scrawl_stderr_{}.log'.format(datetime.date.today().strftime('%Y%m%d'))
 
-logger = logging.getLogger('main')
-handler = logging.FileHandler(os.path.join(LOG_DIR, LOG_FILE))
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+stdout_handler = logging.FileHandler(os.path.join(LOG_DIR, 'scrawl_stdout.log'))
+stderr_handler = logging.FileHandler(os.path.join(LOG_DIR, 'scrawl_stderr.log'))
 formatter = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s %(lineno)d %(message)s'
 )
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.info('ilove you')
+stdout_handler.setFormatter(formatter)
+stderr_handler.setFormatter(formatter)
+
+logger.addHandler(stdout_handler)
+logger.addHandler(stderr_handler)
